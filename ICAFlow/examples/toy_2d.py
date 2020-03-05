0828a683c54dc709c642f93cc60c7785584c9ffc
BilboBannister <jjbannister@gmail.com>
2020-03-05 13:52:05 -0700

    implemented NICE

--- ICAFlow/examples/toy_2d.py
+++ ICAFlow/examples/toy_2d.py
@@ -1,27 +1,34 @@
 import numpy as np
+import tensorflow as tf
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-from ..models.gin import GIN
+from ..models.nice import NICE
 
 def toy_2d():
-    x = get_training_data("Moons", 1000)
-    viz_2d(x)
+    x = get_training_data("Small Normal", 5000)
+    viz_2d(x, "Training Samples")
 
-    gin = GIN()
-    gin.build_model(x.shape[-1], 10, 10)
+    nice = NICE()
+    nice.build_model(x.shape[-1], 0, 1)
 
-    x_hat = gin.transformed_distribution.sample(1000)
-    viz_2d(x_hat)
+    x = nice.base_distribution.sample(1000)
+    viz_2d(x, "Base samples")
 
-    gin.train_model(x, lr=1e-4, n_epochs=1)
+    x = nice.transformed_distribution.sample(1000)
+    viz_2d(x, "Target samples (Pre-Training)")
 
-    x_hat = gin.transformed_distribution.sample(1000)
-    viz_2d(x_hat)
+    nice.train_model(x, lr=1e-3, n_epochs=100, batch_size=256)
+
+    x_hat = nice.transformed_distribution.sample(1000)
+    viz_2d(x_hat, "Target samples (Post-Training)")
+
+    tf.print(nice.scale_values)
 
 
-def viz_2d(data):
+def viz_2d(data, title=""):
     sns.scatterplot(x=data[:,0], y=data[:,1])
+    plt.title(title)
     plt.xlim(-2,2)
     plt.ylim(-2,2)
     plt.show()
@@ -34,6 +41,11 @@ def get_training_data(data_type, n_samples):
         for i in range(n_samples):
             x[i,0] = np.random.uniform(-1,1)
             x[i,1] = np.random.uniform(-1,1)
+
+    if data_type == "Small Normal":
+        for i in range(n_samples):
+            x[i,0] = np.random.normal(0, 0.2)
+            x[i,1] = np.random.normal(0, 0.2)
 
     elif data_type == "Ring":
         for i in range(n_samples):
